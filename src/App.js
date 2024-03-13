ef86b2d14abb94cf6d3a9e185b5c4135e7fde50e
Antho137 <antoniosala7@gmail.com>
2024-03-14 00:55:01 +0100

    hash commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import Home from './components/Home';
 import About from './components/About';
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
 
     return ( 
         <div className="app">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/services" element={<Services />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>     
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Route>
+            </Routes>     
         </div>
     )
 }
