46aa8fa0c91aee413d05df6b8e5e72e03537bd4a
Antonio Sala <59183374+Antho137@users.noreply.github.com>
2024-03-14 02:07:07 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { github } from '/images/github.png';
+import { FaGithub, FaFacebook, FaLinkedinIn, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 
 const Footer = () => {
 	let today = new Date();
@@ -8,13 +8,23 @@ const Footer = () => {
     return (
         <div className='footer'>
             <p>
-                <img src={github} alt="github" />
-                <img src="./images/link.png" alt="linkedin" />
-                <img src="./images/twit.png" alt="twitter" />
-                <img src="./images/whatsapp.png" alt="whatsapp" />
-                <img src="./images/face.png" alt="facebook" />
+	        <a href="https://github.com/Antho137">
+		   <FaGithub className='fas' size={20} />
+	        </a>
+	        <a href="https://www.linkedin.com/in/antonio-sala-antho-b85636256">
+		   <FaLinkedinIn className='fas' size={20} />
+	        </a>
+		<a href="https://twitter.com/antoniosala73">
+		   <FaTwitter className='fas' size={20} />
+		</a>
+		<a href="https://wa.me/244923506453">
+		   <FaWhatsapp className='fas' size={20} />
+		</a>
+		<a href="https://www.facebook.com/antonio.sala.5623">
+		   <FaFacebook className='fas' size={20} />
+		</a>
             </p>
-            <p className='copyright'>Copyright &copy; <span className="year">{year}</span>&#160;  Quick One &#160;</p>
+            <p className='copyright'>Copyright &copy; <span className="year">{year}</span>&#160; Quick One &#160;</p>
         </div>
     );
 }
